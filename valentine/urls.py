b64b28586ce44f1bcdc11724200d1790f6e1bb87
Kowsik Nandagopan D <dkowsikpai@gmail.com>
2021-02-13 02:01:00 +0530

    Admin URL

--- valentine/urls.py
+++ valentine/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('kndcse1822/', admin.site.urls),
     path('', include('app.urls')),
 ]
 
